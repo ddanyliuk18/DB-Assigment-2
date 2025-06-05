5c274993ef3998782ee6846a61663bf8c648db2b
ddanyliuk18 <ddanyliuk@kse.org.ua>
2025-06-05 15:36:30 +0300

    Original query and its Explain added

---
+++ DB-Assigment-2/Scripts/query_versions/original_query.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE athlets01;
+USE athlets01;
+
+SELECT DISTINCT 
+    a.season,
+    a.discipline,
+    a.sex,
+    a.nationality,
+    AVG(a.mark_numeric) AS avg_mark,
+    (
+        SELECT MAX(b.result_score)
+        FROM all_disciplines_combined b
+        WHERE b.discipline = a.discipline
+          AND b.season = a.season
+          AND b.sex = a.sex
+    ) AS max_score
+FROM all_disciplines_combined a
+WHERE a.age_at_event > (
+    SELECT AVG(age_at_event)
+    FROM all_disciplines_combined
+)
+  AND a.mark_numeric IS NOT NULL
+GROUP BY a.season, a.discipline, a.sex, a.nationality;
